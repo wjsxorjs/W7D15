8eab4668bd0d61dfb2bf5d44cf951567d6551bf4
wjsxorjs <wjsxorjs@naver.com>
2024-07-31 15:51:10 +0900

    20240731 NextJS 0.8

---
+++ Ex20240731_Ex1/src/app/pages/test/page.js
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+
+// export default function Test(){
+const Test = () =>{
+    return(
+        <div>
+            <h1>테스트페이지</h1>
+            <article>
+                <Button variant="contained">Contained</Button>
+            </article>
+        </div>
+    );
+}
+
+export default Test;
