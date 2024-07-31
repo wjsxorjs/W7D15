b1c5885e88e47b67601c265dc947ba14ed3e1166
wjsxorjs <wjsxorjs@naver.com>
2024-07-31 15:06:08 +0900

    20240731 NextJS 0.75

---
+++ Ex20240731_Ex1/src/app/pages/apple/page.js
@@ -0,0 +1,26 @@
+import styles from "../../css/menu.css";
+
+function Page(){
+    return(
+        <article id="wrap">
+            <header id="header">
+                <ul id="nav">
+                    <li><a href="" id="apple">&nbsp;</a></li>
+                    <li><a href="">Store</a></li>
+                    <li><a href="">Mac</a></li>
+                    <li><a href="">ipad</a></li>
+                    <li><a href="">iphone</a></li>
+                    <li><a href="">Watch</a></li>
+                    <li><a href="">Vision</a></li>
+                    <li><a href="">Airpods</a></li>
+                    <li><a href="">TV &amp; Home</a></li>
+                    <li><a href="">Entertainment</a></li>
+                    <li><a href="">Accessories</a></li>
+                    <li><a href="">Support</a></li>
+                </ul>
+            </header>
+        </article>
+    );
+}
+
+export default Page;
